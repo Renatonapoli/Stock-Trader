90aef30ddf8bdaa8fca9c20f7e19256c64a33953
Renato Napoli <renato.napoli@nagro.app>
2022-02-15 22:16:00 -0300

    Inserindo ações no portifolio

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -1,9 +1,57 @@
 <template>
-  <h1>Ação (Portifólio)</h1>
+  <v-flex class="mt-4 pr-3 pb-3" xs12 md6 lg4>
+    <v-card class="blue darken-3 white--text">
+      <v-card-title class="headline">
+        <strong
+          >{{ stock.name }}
+          <small
+            >(Preço: {{ stock.price }} | Qtde: {{ stock.quantity }} )
+          </small>
+        </strong>
+      </v-card-title>
+    </v-card>
+    <v-card>
+      <v-container fill-height>
+        <v-text-field
+          label="Quantidade"
+          type="number"
+          v-model.number="quantity"
+        />
+        <v-btn
+          class="blue darken-3 white--text"
+          :disable="quantity <= 0 || !Number.isInteger(quantity)"
+          @click="sellStock"
+          >Vender</v-btn
+        >
+      </v-container>
+    </v-card>
+  </v-flex>
 </template>
 
 <script>
-export default {};
+import { mapActions } from "vuex";
+
+export default {
+  props: ["stock"],
+  data() {
+    return {
+      quantity: 0,
+    };
+  },
+  methods: {
+    ...mapActions({ sellStockAction: "sellStock" }),
+    sellStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity,
+      };
+      this.sellStockAction(order);
+      // this.$store.dispatch("sellStock", order);
+      this.quantity = 0;
+    },
+  },
+};
 </script>
 
 <style></style>
